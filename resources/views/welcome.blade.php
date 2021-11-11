f15844b430b1b7c66845c82c2f357d47bd21b4c8
beibei28845 <1097109986@qq.com>
2021-11-11 05:55:49 +0000

    修改ENV

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -26,7 +26,7 @@
         .content {
             text-align: center;
             display: inline-flex;
-            color: darkred;
+            color: darkred; background: aqua ;
         }
 
         .title {
