671eb1b83b0de52b556a334c5ace1392b28a9105
beibei28845 <1097109986@qq.com>
2021-11-28 10:28:26 +0000

    用户注册

--- app/Http/Controllers/UsersController.php
+++ app/Http/Controllers/UsersController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers;
 
 use App\Models\User;
 use Illuminate\Http\Request;
+use mysql_xdevapi\Session;
 
 class UsersController extends Controller
 {
@@ -24,6 +25,14 @@ class UsersController extends Controller
             'email' => 'required|email|unique:users|max:255',
             'password' => 'required|confirmed|min:6'
         ]);
-        return;
+
+        $user = User::create([
+            'name' => $request->name,
+            'email' => $request->email,
+            'password' => bcrypt($request->password),
+        ]);
+
+        session()->flash('success', 'hello,world');
+        return redirect()->route('users.show', [$user]);
     }
 }
