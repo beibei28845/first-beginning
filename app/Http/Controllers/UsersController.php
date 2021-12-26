f20bd80579d576eae23684c108ab53423b069c60
beibei28845 <1097109986@qq.com>
2021-12-26 06:00:28 +0000

    用户更改资料

--- app/Http/Controllers/UsersController.php
+++ app/Http/Controllers/UsersController.php
@@ -37,4 +37,27 @@ class UsersController extends Controller
         session()->flash('success', '欢迎回来');
         return redirect()->route('users.show', [$user]);
     }
+
+    public function edit(User $user)
+    {
+        return view('users.edit', compact('user'));
+    }
+
+    public function update(User $user, Request $request)
+    {
+        $this->validate($request, [
+            'name' => 'required|max:50',
+            'password' => 'nullable|confirmed|min:6',
+        ]);
+
+        $data = [];
+        $data['name'] = $request->name;
+        if ($request->password) {
+            $data['password'] = bcrypt($request->password);
+        }
+        $user->update($data);
+
+        Session()->flash('success', '个人资料更新成功');
+        return redirect()->route('users.show', $user);
+    }
 }
